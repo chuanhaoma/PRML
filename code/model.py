61111b3a422e534f8fa794905d68f8dc227c8470
MaChuanhao <vilriginmch@sina.com>
2025-06-28 08:53:51 +0800

    Update. MaChuanhao.

--- code/model.py
+++ code/model.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 import torchvision
 
@@ -20,7 +21,40 @@ class ViTForPollenClassification(nn.Module):
     
     def forward(self, x):
         return self.vit(x)
+
+# 获取融合注意力
+class ViTWithCrossAttnForPollenClassification(nn.Module):
+    def __init__(self, all_weight=False, num_classes=73):
+        super().__init__()
+        pretrained_vit_weights = torchvision.models.ViT_B_16_Weights.DEFAULT
+        self.vit = torchvision.models.vit_b_16(weights=pretrained_vit_weights)
+
+        for block in self.vit.encoder.layers: # 开启自注意力
+            block.return_attention = True
+
+        for parameter in self.vit.parameters(): # 冻结参数
+            parameter.requires_grad = all_weight
+
+        self.vit.heads = nn.Linear(in_features=self.vit.hidden_dim, out_features=num_classes)
     
+    def forward(self, x):
+        x = self.vit._process_input(x)
+
+        n = x.shape[0]
+        batch_class_token = self.vit.class_token.expand(n, -1, -1)
+
+        x = torch.cat([batch_class_token, x], dim=1)
+
+        attn_maps = []  # 存储各层注意力图
+        for blk in self.vit.encoder.layers:
+            blk.return_attention = True
+            x, attn = blk(x)
+            attn_maps.append(attn)
+        
+        # 融合最后3层注意力图（加权平均）
+        fused_attn = 0.4*attn_maps[-1] + 0.3*attn_maps[-2] + 0.3*attn_maps[-3]
+        return self.vit.heads(x), fused_attn  # 返回分类结果和融合注意力
+
 # 带预训练权重的Vision Transformer L-16
 class ViTLForPollenClassification(nn.Module):
     def __init__(self, all_weight=False, num_classes=73):
